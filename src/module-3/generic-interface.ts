bdb010c8e2193ca253f31215dd67d9d9c14aa1a3
ThakurSaad <thakursaad613@gmail.com>
2023-05-03 16:07:44 +0600

    3.4 generic in interface

---
+++ src/module-3/generic-interface.ts
@@ -0,0 +1,44 @@
+interface CrushInterface<T, U = null> {
+  name: string;
+  husband: T;
+  kids?: U;
+}
+
+const crush01: CrushInterface<number, undefined | null> = {
+  name: "lalalalisa",
+  husband: 0,
+};
+
+const crush02: CrushInterface<boolean> = {
+  name: "lalalalisa",
+  husband: false,
+};
+
+interface IHusband {
+  name: string;
+  salary: number;
+}
+
+const crush03: CrushInterface<IHusband, number> = {
+  name: "lalalalisa",
+  husband: {
+    name: "Thakur",
+    salary: 1000000,
+  },
+  kids: 3,
+};
+
+const crush04: CrushInterface<
+  { name: string; age: number },
+  { name: string; age: number }
+> = {
+  name: "Jenny",
+  husband: {
+    name: "Thakur",
+    age: 22,
+  },
+  kids: {
+    name: "Nezuko",
+    age: 21,
+  },
+};
