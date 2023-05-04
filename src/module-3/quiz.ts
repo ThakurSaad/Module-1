03840a8832c2095acc9ed47c0ff9e0c863e473a5
ThakurSaad <thakursaad613@gmail.com>
2023-05-04 19:33:38 +0600

    quiz done

---
+++ src/module-3/quiz.ts
@@ -0,0 +1,29 @@
+function getArrayItem<T, K extends keyof T>(
+  arr: T[],
+  index: number,
+  key: K
+): T[K] {
+  const item = arr[index];
+
+  //   console.log(item, "\n", item[key]);
+
+  return item[key];
+}
+
+const users = [
+  { name: "John", age: 30 },
+  { name: "Mary", age: 25 },
+];
+
+// getArrayItem(users, 0, "name");
+
+// ---------------------
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function fullName<T extends Person>(person: T): string {
+  return `${person.firstName} ${person.lastName}`;
+}
