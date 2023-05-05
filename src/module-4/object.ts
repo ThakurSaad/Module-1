35314391b1d28866ccb8b89fb725b36be8c43343
ThakurSaad <thakursaad613@gmail.com>
2023-05-05 16:47:23 +0600

    4.2 create class, parameter properties

---
+++ src/module-4/object.ts
@@ -0,0 +1,45 @@
+/*
+class Animal {
+  // 1. defining class properties
+  public name: string;
+  public species: string;
+  public sound: string;
+
+  constructor(name: string, species: string, sound: string) {
+    // 2. initializing parameters
+
+    // 3. assigning parameters to class properties
+    // this.name is class property
+    // name is the initialized parameter
+
+    this.name = name;
+    this.species = species;
+    this.sound = sound;
+  }
+
+  public makeSound() {
+    console.log(`This ${this.name} says ${this.sound}`);
+  }
+}
+*/
+
+// --------------------------------------
+// using parameter properties
+
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string
+  ) {}
+
+  public makeSound() {
+    console.log(`The ${this.name} says ${this.sound}`);
+  }
+}
+// creating instance
+const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+const cat = new Animal("Mini", "cat", "meow");
+
+dog.makeSound();
+cat.makeSound();
