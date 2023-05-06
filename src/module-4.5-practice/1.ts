0d6e26cf6c7b122bff2bb661906fd59d1b77fcef
ThakurSaad <thakursaad613@gmail.com>
2023-05-06 18:26:03 +0600

    practice question 1 solved

---
+++ src/module-4.5-practice/1.ts
@@ -0,0 +1,10 @@
+// Convert the following JavaScript array into a TypeScript tuple
+// userInfo = [101, "Ema", "John", true,  , "2023"];
+
+const myTuple: [number, string, string, boolean, string] = [
+  101,
+  "Ema",
+  "John",
+  true,
+  "2023",
+];
